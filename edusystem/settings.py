5acb9e04ec09ff8357eece1e0d3bb21deff64037
Zulayho <you@example.com>
2026-05-21 18:27:15 +0500

    Like

--- edusystem/settings.py
+++ edusystem/settings.py
@@ -29,13 +29,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+    # "unfold",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses'
+    'courses',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
